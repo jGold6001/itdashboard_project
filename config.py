e316eda7e6231e4c2c483a42edb0762b44e1b495
Viacheslav Zlatov <jgold6001@gmail.com>
2021-02-07 15:46:13 +0200

    web process - finished. Update project structure

--- config.py
+++ config.py
@@ -4,6 +4,11 @@
 URL = "https://itdashboard.gov/"
 AGENCY_NAME = 'Department of the Interior'
 
+# Excel
+EXCEL_OUTPUT_NAME = "output_data.xlsx"
+AGENCIES_SHEET = "Agencies"
+INDIVIDUAL_INVESTMENTS_SHEET = "Individual Investments"
+
 # Directories
 PATH_TO_OUTPUT_DIR = "output"
 PDFS_DIR_NAME = "pdfs"
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.utility.common import Common
 from src.webprocess.webprocess import WebProcess
+from src.excelprocess.excelprocess import ExcelProcess
 import config
 
 
@@ -13,19 +14,24 @@ def main():
         directories = Common.check_and_create_directories()
 
         # tasks
-        web_process = WebProcess(website_url, directories)
-        web_process.set_and_open_the_website()
+        # web_process = WebProcess(website_url, directories)
+        # web_process.set_and_open_the_website()
 
         # web_process.scrape_and_get_agencies_data()
         # web_process.select_agency(name=agency_name)
         # web_process.scrape_data_table_of_agency()
         # web_process.download_pdf_files()
-        web_process.test_method()
 
-    except Exception as ex:
-        print("Unexpected error - " + str(ex))
+        print("test excel")
+        excel = ExcelProcess(directories["excel"])
+        excel.create_file()
+        excel.test_method()
+        excel.save_and_close_file()
+
+    # except Exception as ex:
+    #     print("Unexpected error - " + str(ex))
     finally:
-        web_process.close_the_website()
+        # web_process.close_the_website()
         print("===finish process===")
 
 
